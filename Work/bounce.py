efaf9a3452e28b480754e99ae80a31cadd04e0ce
nv2lt <nv2lt@gitlab.com>
2020-11-10 00:00:39 +0200

    initial

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,8 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100.0
+
+for i in range(10):
+     height = round((height * 3) / 5, 4)
+     print(i+1, height)
--- Work/mortgage.py
+++ Work/mortgage.py
@@ -1,3 +1,32 @@
 # mortgage.py
 #
 # Exercise 1.7
+principal = 500000.0
+rate = 0.05
+payment = 2684.11
+total_paid = 0.0
+extra_payment = 1000.0
+extra_payment_start_month = 61
+extra_payment_end_month = 108
+months = 1
+
+while principal >0:
+    if months >= extra_payment_start_month and months <= extra_payment_end_month:
+        cur_payment = payment + extra_payment
+    else:
+        cur_payment = payment
+
+    if (principal * (1+rate/12)) >= cur_payment: 
+        principal = principal * (1+rate/12) - cur_payment
+        total_paid = total_paid + cur_payment
+    else: 
+        total_paid = total_paid + principal
+        principal = 0
+
+
+    print(f'{months} {total_paid:>12,.2f} {principal:>12,.2f}')
+    
+    months = months + 1
+
+print(f'Total paid: {total_paid:>5,.2f}')
+print(f'Months: {months-1}')
