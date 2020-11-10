1a5528ef5128412e19b909d5859c3469c950e570
nv2lt <nv2lt@gitlab.com>
2020-11-10 19:12:54 +0200

    ewew

--- Work/pcost.py
+++ Work/pcost.py
@@ -25,11 +25,14 @@ def portfolio_cost(filename):
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
-        for row in rows:
+        for rowno, row in enumerate(rows, start=1):
+            record = dict(zip(headers, row))
             try:
-                total_cost = total_cost + float(row[1]) * float(row[2])
-            except:
-                print(f'Parse error in line {row}')
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                total_cost += nshares * price
+            except ValueError:
+                print(f'Row {rowno}: Bad row {row}')
     return total_cost
 
 
--- Work/report.py
+++ Work/report.py
@@ -13,10 +13,17 @@ def read_portfolio(filename):
     with open(filename, 'rt') as f:
         rows = csv.reader(f)
         headers = next(rows)
-        for row in rows:
+        for rowno, row in enumerate(rows):
+            record = dict(zip(headers, row))
+            try:
+                nshares = int(record['shares'])
+                price = float(record['price'])
+                name = record['name']
+            except ValueError:
+                print(f'Row {rowno}: Bad row: {row}')
             #holding = (row[0], int(row[1]), float(row[2]))
-            holding = {'name': row[0], 'shares': int(row[1]), 'price': float(row[2])}
-            portfolio.append(holding)
+            #holding = {'name': row[0], 'shares': int(row[1]), 'price': float(row[2])}
+            portfolio.append({'name': name, 'shares': nshares, 'price': price})
     return portfolio
 
 def read_prices(filename):
